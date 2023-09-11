8fd1e2b554345a7194cbdd319184a9ef28265518
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-09-11 14:55:35 -0400

    update index.js with selection options and table

--- index.js
+++ index.js
@@ -1,3 +1,60 @@
-const inquire = require("inquire")
+const inquirer = require("inquirer")
 const mysql = require("mysql2")
 const {printTable} = require("console-table-printer")
+
+require("dotenv").config()
+
+const db= mysql.createConnection({
+    host: "localhost",
+    user:process.env.DB_USER,
+    password:process.env.DV_PASSWORD,
+    database:process.env.DB_NAME,
+    port:3306
+})
+db.connect(()=>{
+   mainMenu()
+})
+//add figlet
+
+//  view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+function mainMenu(){
+    inquirer.prompt({
+        type:"list",
+        message:"What would you like to do?",
+        name:"selection",
+        choices: ["view all departments","view all roles","view all employees","add a department","add a role","add an employee","update an employee role"]
+    })
+    .then(answer =>{
+        if(answer.selection ==="view all employees"){
+            viewEmployees()
+
+        }
+        else if (answer.selection==="add an employee"){
+            addEmployee()
+
+        }
+        else if (answer.selection === "update an employee role"){
+            updateEmployeerole()
+
+        }
+    })
+}
+
+function viewEmployees(){
+    db.query(`SELECT employee.id, employee.first_name, employee.last_name, title, name as department, salary, CONCAT( bosses.first_name,' ' ,bosses.last_name) as manager from employee
+    left join role on employee.role_id = role.id
+    left join department on department.id = role.department_id
+    left join employee as bosses on employee.manager_id = bosses.id;`, (err,data)=>{
+        printTable(data)
+        mainMenu()
+    })
+
+}
+
+function addEmployee(){
+
+}
+
+function  updateEmployeerole(){
+
+}
